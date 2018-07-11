0b7677954d6bd526233529bc8434e5d4a852734c
AvyayAgarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2018-07-11 14:00:57 -0400

    Added address and coordinate detection using Google Maps API

---
+++ app.js
@@ -0,0 +1,25 @@
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  console.log(argv.address);
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
+});
