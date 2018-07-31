7a64c502ee72e14c0a5ca41ead668b495a621708
Avyay Agarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2018-07-31 09:14:45 -0400

    Broken code committed for records

--- app.js
+++ app.js
@@ -5,6 +5,62 @@ const bodyParser = require('body-parser');
 const geocode = require('./geocode');
 const weather = require('./weather');
 
+async function ab(req) {
+  await geocode.geocodeAddress(req.body.address, async function (addressError, addressResults) {
+    //var result= '\nResult\n';
+    // console.log("address result", addressResults);
+    // console.log("Passing address:", req.body.address);
+    console.log(); //To provide initial spacing int he command line regardless of the output
+    if (addressError) {
+      //console.log(`Address Error: ${addressError}`);
+      result = result + `Address Error: ${addressError}\n`;
+      return result;
+    } else {
+      await weather.weatherInfo(addressResults, (weatherError, weatherResults) => {
+        if(weatherError) {
+          //console.log(`Weather Error: ${weatherError}`);
+          result = result + `Weather Error: ${weatherError}\n`;
+
+        } else {
+          //console.log(`The weather in ${addressResults.address} is currently ${weatherResults.weather}.`);
+          //console.log(weatherResults.dayForecast);
+          result = result + `The weather in ${addressResults.address} is currently ${weatherResults.weather}. \n${weatherResults.dayForecast} \n`;
+          // console.log("ersez",result);
+          if(weatherResults.temperature !== weatherResults.realFeel) {
+            //console.log(`It is ${weatherResults.temperature}° but it feels like ${weatherResults.realFeel}°.`);
+            result = result + `It is ${weatherResults.temperature}° but it feels like ${weatherResults.realFeel}°.\n`;
+          } else {
+            //console.log(`The present temperature is ${weatherResults.temperature}°.`);
+            result = result + `The present temperature is ${weatherResults.temperature}°.\n`;
+          }
+  
+          if(weatherResults.precipitation !== undefined) { //If there is a chance of some type of precipitation
+            //console.log(`There is a ${Math.round(weatherResults.probability*100)}% chance of ${weatherResults.precipitation} and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`);
+            result = result + `There is a ${Math.round(weatherResults.probability*100)}% chance of ${weatherResults.precipitation} and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
+          } else { //If there is not precipitation
+            //console.log(`There is a ${Math.round(weatherResults.probability*100)}% chance of precipitation and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`);
+            result = result + `There is a ${Math.round(weatherResults.probability*100)}% chance of precipitation and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
+          }
+  
+          if(weatherResults.alert !== undefined) {
+            if(weatherResults.alert.title!== undefined) {
+              //console.log(`There is a ${weatherResults.alert.title}.\n`);
+              result = result + `There is a ${weatherResults.alert.title}.\n`;
+            }
+          }
+        }
+        console.log("mnbvctia", result, "end");
+        return result;
+        // return new Promise(resolve => {
+        //   console.log("gadha", result)
+        //   resolve(result);
+        // });
+      });
+    }  
+  })
+}
+
+var result= `\-----\n`;
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -14,75 +70,87 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
 
 
-var result = `\nTesting\n`;
-// geocode.geocodeAddress(argv.address, (addressError, addressResults) => {
+  app.get('/', (req, res) => {
+    res.render('index', {
+      weatherPrint: result,
+    });
+  });
+
+// var fetchResults = geocode.geocodeAddress("Toronto", (addressError, addressResults) => {
 //   console.log(); //To provide initial spacing int he command line regardless of the output
 //   if (addressError) {
 //     console.log(`Address Error: ${addressError}`);
-//     result = result + `Address Error: ${addressError}\n`;
+//     result= result+ `Address Error: ${addressError}\n`;
 //   } else {
 //     weather.weatherInfo(addressResults, (weatherError, weatherResults) => {
 //       if(weatherError) {
 //         console.log(`Weather Error: ${weatherError}`);
-//         result = result + `Weather Error: ${weatherError}\n`;
+//         result= result+ `Weather Error: ${weatherError}\n`;
 //       } else {
 //         console.log(`The weather in ${addressResults.address} is currently ${weatherResults.weather}.`);
 //         console.log(weatherResults.dayForecast);
-//         result = result + `The weather in ${addressResults.address} is currently ${weatherResults.weather}. \n${weatherResults.dayForecast} \n`;
+//         result= result+ `The weather in ${addressResults.address} is currently ${weatherResults.weather}. \n${weatherResults.dayForecast} \n`;
 
 //         if(weatherResults.temperature !== weatherResults.realFeel) {
 //           console.log(`It is ${weatherResults.temperature}° but it feels like ${weatherResults.realFeel}°.`);
-//           result = result + `It is ${weatherResults.temperature}° but it feels like ${weatherResults.realFeel}°.\n`;
+//           result= result+ `It is ${weatherResults.temperature}° but it feels like ${weatherResults.realFeel}°.\n`;
 //         } else {
 //           console.log(`The present temperature is ${weatherResults.temperature}°.`);
-//           result = result + `The present temperature is ${weatherResults.temperature}°.\n`;
+//           result= result+ `The present temperature is ${weatherResults.temperature}°.\n`;
 //         }
 
 //         if(weatherResults.precipitation !== undefined) { //If there is a chance of some type of precipitation
 //           console.log(`There is a ${Math.round(weatherResults.probability*100)}% chance of ${weatherResults.precipitation} and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`);
-//           result = result + `There is a ${Math.round(weatherResults.probability*100)}% chance of ${weatherResults.precipitation} and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
+//           result= result+ `There is a ${Math.round(weatherResults.probability*100)}% chance of ${weatherResults.precipitation} and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
 //         } else { //If there is not precipitation
 //           console.log(`There is a ${Math.round(weatherResults.probability*100)}% chance of precipitation and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`);
-//           result = result + `There is a ${Math.round(weatherResults.probability*100)}% chance of precipitation and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
+//           result= result+ `There is a ${Math.round(weatherResults.probability*100)}% chance of precipitation and humidity is at ${Math.round(weatherResults.humidity*100)}%.\n`;
 //         }
 
 //         if(weatherResults.alert !== undefined) {
 //           if(weatherResults.alert.title!== undefined) {
 //             console.log(`There is a ${weatherResults.alert.title}.\n`);
-//             result = result + `There is a ${weatherResults.alert.title}.\n`;
+//             result= result+ `There is a ${weatherResults.alert.title}.\n`;
 //           }
 //         }
 //       }
 //     });
 //   }
+//   return result;
 // });
+const tf = async (req) => {
+  return await ab(req);
+}
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    weatherPrint: 'Testing',
-  });
-});
-
-app.post('/show', (req,res) => {
-  res.render('index', {
-    weatherPrint: req.body.address
-  });
+app.post('/show', async (req,res) => {
+  // 
+  // (async () => console.log("lund", ab(req)))()
+  var a  = tf(req).then(response => {
+    console.log("gyug", a, "vvyvuu");
+    res.render('index', {
+      weatherPrint: response
+    });
+  }
+);
+  // setTimeout(function(){ console.log("avyay abhi bhi chutiya hai") }, 10000);
+  // console.log("a",a)
+  
 });
 
 app.listen(3000, () => {
-  console.log("\n Server is up on port 3000\n");
+  console.log("\nServer is up on port 3000\n");
 });
--- geocode.js
+++ geocode.js
@@ -1,9 +1,9 @@
 const request = require('request');
 
-var geocodeAddress = (address, callback) => {
+var geocodeAddress = async (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
-  request({
+  await request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
@@ -12,6 +12,7 @@ var geocodeAddress = (address, callback) => {
     } else if (body.status === 'ZERO_RESULTS') {
       callback('Unable to find that address.');
     } else if (body.status === 'OK') {
+      // console.log(body.results, "nii");
       callback(undefined, {
         address: body.results[0].formatted_address,
         latitude: body.results[0].geometry.location.lat,
